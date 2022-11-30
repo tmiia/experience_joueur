27db9ab635cfaa408071d965d1c601b3615eeeaa
Olivia206 <olivia.simonet@mmibordeaux.com>
2022-11-30 10:22:39 +0100

    added game manager class

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance = null;
+
+    // Variables globales
+
+    public static int choice = 0;
+    public static int level = 0;
+    public static int rate = 0;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            instance = this;
+        } else
+        {
+            Destroy(gameObject);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public static void ChangeScene (int sceneBuildIndex)
+    {
+        SceneManager.LoadScene(sceneBuildIndex);
+    }
+}
